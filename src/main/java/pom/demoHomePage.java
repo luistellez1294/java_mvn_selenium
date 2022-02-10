9799dc6408c1461794233062072a66d23fca35ab
luistellez1294 <ltellez@deloittemx.com>
2022-02-10 14:46:37 -0600

    Added more methods fo demoHP for my test case

--- src/main/java/pom/demoHomePage.java
+++ src/main/java/pom/demoHomePage.java
@@ -6,43 +6,97 @@ import utility.commonMethods;
 
 import java.util.Hashtable;
 
-public class googleHomePage extends seleniumActions{
-
-    private commonMethods common;
-
-    Hashtable<String, String> validationElements = new Hashtable<String, String>();
-
-    public void setValidationElements(){
-        validationElements.put("searchBox", "xpath://input[@class='gLFyf gsfi']");
-    }
-
-    //private String searchBox = "xpath://input[@class='gLFyf gsfi']";
-    private String googleLogo = "class:lnXdpd";
-    private String gmailLink = "xpath://a[contains(@href,'mail/&')]";
-
-    public googleHomePage(WebDriver driver) {
+public class demoHomePage extends seleniumActions{
+    //Creating constructors for class
+    public demoHomePage(WebDriver driver) {
         super(driver);
         setValidationElements();
+        setInputTextElements();
+        setClickableElements();
     }
 
-    public googleHomePage() {
+    public demoHomePage() {
         setValidationElements();
+        setInputTextElements();
+        setClickableElements();
     }
 
-    public String getGmailLinkText(WebDriver driver) {
-        waitUntilElementIsVisible(driver, gmailLink, 2);
-        return getTextFromElement(driver, gmailLink);
+
+
+    //Creating common object to access dictionaries dynamically
+    commonMethods common = new commonMethods();
+
+    Hashtable<String, String> validationElements = new Hashtable<>();
+    Hashtable<String, String> inputTextElements = new Hashtable<>();
+    Hashtable<String, String> clickableElements = new Hashtable<>();
+
+
+/*
+    Declaring dictionary for elements by type on Web for their ease of management
+    Setters and getters for the dictionary, in an organized way.
+
+
+ */
+
+    public void setValidationElements() {
+        validationElements.put("pageLogo", "xpath://img[@class='logo img-responsive']");
+        validationElements.put("shop-phone", "class:shop-phone");
     }
 
-    public void validateGoogleLogoIsPresent(WebDriver driver){
-        waitUntilElementIsVisible(driver, googleLogo, 15);
+    public void setClickableElements() {
+        clickableElements.put("searchButton", "name:submit_search");
+        clickableElements.put("contactUs", "class:login");
+        clickableElements.put("signIn", "id:contact-link");
     }
 
-    public void inputTextOnGoogleSearchBox(WebDriver driver, String text) {
-        String element = common.getFromDictionary(validationElements, "searchBox");
+    public void setInputTextElements() {
+        inputTextElements.put("searchBox", "id:search_query_top");
+    }
+
+    private Hashtable<String, String> getValidationElements(){
+        return validationElements;
+    }
+
+    private Hashtable<String, String> getInputTextElements(){
+        inputTextElements.put("searchBox", "id:search_query_top");
+        return inputTextElements;
+    }
+
+    private Hashtable <String, String> getClickableElements() {
+        return clickableElements;
+    }
+
+/*
+    Creating the methods we will use on home page.
+
+
+ */
+
+
+    public void validateLogoIsPresent(WebDriver driver){
+        String element = common.getFromDictionary(getValidationElements(), "pageLogo");
+        waitUntilElementIsVisible(driver, element, 15);
+    }
+
+    public void inputTextOnSearchBox(WebDriver driver, String text) {
+        String element = common.getFromDictionary(getInputTextElements(), "searchBox");
 
         waitUntilElementIsVisible(driver, element, 15);
         clearElement(driver, element);
         inputTextIntoElement(driver, element, text);
+    }
+
+    public void clickOnContactUs(WebDriver driver) {
+        String element = common.getFromDictionary(getClickableElements(), "contactUs");
+
+        waitUntilElementIsVisible(driver, element, 30);
+        clickElement(driver, element);
+    }
+
+    public void clickOnSearchButton(WebDriver driver) {
+        String element = common.getFromDictionary(getClickableElements(), "searchButton");
+
+        waitUntilElementIsVisible(driver, element, 30);
+        clickElement(driver, element);
     }
 }
